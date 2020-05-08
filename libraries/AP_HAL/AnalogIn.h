73a7e2097398619fad48cf7dad8fa20ac871c2e1
Michael du Breuil <wicked.shell.scripts@gmail.com>
2020-05-08 00:19:17 -0700

    AP_HAL: Remove unneeded AnalogIn voltage_latest() and read_latest()

--- libraries/AP_HAL/AnalogIn.h
+++ libraries/AP_HAL/AnalogIn.h
@@ -7,16 +7,11 @@
 class AP_HAL::AnalogSource {
 public:
     virtual float read_average() = 0;
-    virtual float read_latest() = 0;
     virtual void set_pin(uint8_t p) = 0;
 
     // return a voltage from 0.0 to 5.0V, scaled
     // against a reference voltage
     virtual float voltage_average() = 0;
-
-    // return a voltage from 0.0 to 5.0V, scaled
-    // against a reference voltage
-    virtual float voltage_latest() = 0;
 
     // return a voltage from 0.0 to 5.0V, assuming a ratiometric
     // sensor
